1fd2730738d635a6a9bb79a8e650f892f92f4aee
Thisura Manjitha Samarakoon <thissamarakoon@gmail.com>
2023-01-07 11:57:27 +0530

    Day1

---
+++ my-work/1-DSA-in-C++/Chapter-1/practice.cpp
@@ -0,0 +1,12 @@
+#include <cstdlib>
+#include <iostream>
+
+int main() {
+    /*int x, y;
+    std::cout << "Please enter two numbers: ";
+    std::cin >> x >> y;
+    int sum = x + y;
+    std::cout << "Their sum is " << sum << std::endl;*/
+    std::cout << sizeof(int) << std::endl;
+    return EXIT_SUCCESS;
+}
