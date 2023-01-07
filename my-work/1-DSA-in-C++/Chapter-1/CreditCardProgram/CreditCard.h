eeb3a37ada027cf4dd3fba8b34c23e0db511cfd6
Thisura Manjitha Samarakoon <thissamarakoon@gmail.com>
2023-01-07 20:06:15 +0530

    chapter1 - created CreditCard header file.

---
+++ my-work/1-DSA-in-C++/Chapter-1/CreditCardProgram/CreditCard.h
@@ -0,0 +1,27 @@
+#ifndef CREDIT_CARD_H
+#define CREDIT_CARD_H
+
+#include <ostream>
+#include <string>
+
+class CreditCard {
+public:
+  CreditCard(const std::string &num, const std::string &nm, int limit,
+             double balance = 0);
+  std::string getNumber() const { return number; }
+  std::string getName() const { return name; }
+  int getLimit() const { return limit; }
+  double getBalance() const { return balance; }
+  bool chargeIt(double price);
+  void makePayment(double payment);
+
+private:
+  std::string number;
+  std::string name;
+  int limit;
+  double balance;
+};
+
+std::ostream &operator<<(std::ostream &out, const CreditCard &creditCard);
+
+#endif
